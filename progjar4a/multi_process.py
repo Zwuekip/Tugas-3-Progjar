3481b843f70b4f3a0e8cc4f4e6fde36e3bd92cb6
Nodas uziel <61287359+Zwuekip@users.noreply.github.com>
2021-06-06 18:42:17 +0700

    update multi procesing

--- progjar4a/multi_process.py
+++ progjar4a/multi_process.py
@@ -5,17 +5,16 @@ from multiprocessing import Process
 
 def download_semua():
     texec = dict()
-    # urls = get_url_list()
     catat_awal = datetime.datetime.now()
     namafile = 'pokijan.jpg'
     for k in range(100):
-        print(f"mendownload {urls[k]}")
+        print(f"mendownload GET {[k]}")
         waktu = time.time()
         #bagian ini merupakan bagian yang mengistruksikan eksekusi fungsi download gambar secara multiprocess
         texec[k] = Process(target=remote_get, args=(namafile,))
         texec[k].start()
     #setelah menyelesaikan tugasnya, dikembalikan ke main process dengan join
-    for k in urls:
+    for k in range(100):
         texec[k].join()
     catat_akhir = datetime.datetime.now()
     selesai = catat_akhir - catat_awal
